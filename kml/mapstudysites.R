b0dc0dbafb847616bebd1dac54556556af9a006c
Scott Chamberlain <myrmecocystus@gmail.com>
2012-09-11 19:24:13 -0700

    edited kml example

--- kml/mapstudysites.R
+++ kml/mapstudysites.R
@@ -1,6 +1,6 @@
 ##  map of study sites in pollinator-seed predator study
-require(XML); require(plyr)
-temp <- xmlToList("helianthussites_gmaps.kml")[25:46]
+require(XML); require(plyr); require(RCurl)
+temp <- xmlToList(getURL("https://raw.github.com/SChamberlain/work/master/kml/helianthussites_gmaps.kml"))[25:46]
 getit <- function(x) {
   a <- x[[1]]
   dd <- as.numeric(str_split(x[[length(x)]], ",")[[1]][1:2])
