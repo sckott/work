23537ca58a37067d37f705d52286adf6543fe0aa
Scott Chamberlain <myrmecocystus@gmail.com>
2012-09-14 14:26:08 -0700

    edited code to make Journal of Ecology stast post

--- jecol/blogstats.r
+++ jecol/blogstats.r
@@ -1,12 +1,12 @@
-library(XML); library(stringr); library(ggplot2)
-temp <- htmlParse("/Mac/JEcol_blog/data_posts/blogstats/sitestats.html")
+library(XML); library(stringr); library(ggplot2); library(RCurl)
+temp <- htmlParse(getURL("https://raw.github.com/SChamberlain/work/master/jecol/sitestats.html"))
 temp2 <- xpathSApply(temp, "//td", xmlValue)
 temp3 <- temp2[sapply(temp2, function(x) nchar(x), USE.NAMES=F) > 0]
 
 titles <- temp3[seq(1, length(temp3), 2)]
 views <- temp3[seq(2, length(temp3), 2)]
 dat <- data.frame(titles=titles, views=views)
-dat <- dat[!dat$titles == "Home page / Archives",] # remove home page views
+dat <- dat[!dat$titles %in% c("Home page / Archives","About","Contributors"),] # remove home page views
 dat$views <- as.numeric(str_replace_all(as.character(dat$views), ",", ""))
 dat <- dat[order(dat$views, decreasing=T),]
 
