628217f05bc47a7529ed7c918444973ee6a377fb
Scott Chamberlain <myrmecocystus@gmail.com>
2012-09-14 14:00:20 -0700

    added code for jecol blog post

---
+++ jecol/blogstats.r
@@ -0,0 +1,19 @@
+library(XML); library(stringr); library(ggplot2)
+temp <- htmlParse("/Mac/JEcol_blog/data_posts/blogstats/sitestats.html")
+temp2 <- xpathSApply(temp, "//td", xmlValue)
+temp3 <- temp2[sapply(temp2, function(x) nchar(x), USE.NAMES=F) > 0]
+
+titles <- temp3[seq(1, length(temp3), 2)]
+views <- temp3[seq(2, length(temp3), 2)]
+dat <- data.frame(titles=titles, views=views)
+dat <- dat[!dat$titles == "Home page / Archives",] # remove home page views
+dat$views <- as.numeric(str_replace_all(as.character(dat$views), ",", ""))
+dat <- dat[order(dat$views, decreasing=T),]
+
+ggplot(dat, aes(reorder(titles, views), views)) +
+	geom_point(size = 3) +
+	coord_flip() +
+	theme_bw() +
+	labs(x = "", y = "Views") + 
+	theme(panel.grid.major = element_blank(), panel.grid.minor=element_blank(), 
+				axis.text.x = element_text(size=18), axis.title.x = element_text(size=18))
