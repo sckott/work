90bb41198791a220191316a28a79e5dc461e2f64
Scott Chamberlain <myrmecocystus@gmail.com>
2012-10-30 11:20:00 -0700

    added folder for vamosi_heard project

---
+++ vamosi_heard/vamosi_heard.r
@@ -0,0 +1,16 @@
+##### 
+### Vamosi-Heard project
+## Sync file on git repo to dropbox whenever necessary
+# system("cp /Users/ScottMac/github/SChamberlain/work/vamosi_heard/vamosi_heard.r /Users/ScottMac/Dropbox/Vamosi_Heard/code")
+
+#####
+# Read in data, clean
+
+#####
+# Search for and download sequences
+
+#####
+# 
+
+#####
+# 
